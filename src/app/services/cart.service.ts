889483c1cc5ee8dd1464e5a5c27a36f8fe675dd7
Tanuj Chowdhury <chowdhury.tanuj@gmail.com>
2023-06-06 16:00:12 -0500

    handle browser refresh

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -11,7 +11,19 @@ export class CartService {
     totalPrice: Subject<number> = new BehaviorSubject<number>(0);
     totalQuantity: Subject<number> = new BehaviorSubject<number>(0);
 
-    constructor() {}
+    storage: Storage = sessionStorage;
+
+    constructor() {
+        // read data from storage
+        let data = this.storage.getItem('cartItems');
+
+        if (data != null) {
+            this.cartItems = JSON.parse(data);
+
+            // compute totals based on the data that is read from storage
+            this.computeCartTotals();
+        }
+    }
 
     addToCart(theCartItem: CartItem) {
         // check if we already have the item in our cart
@@ -58,6 +70,13 @@ export class CartService {
 
         // log cart data just for debugging purposes
         this.logCartData(totalPriceValue, totalQuantityValue);
+
+        // persist cart data
+        this.persistCartItems();
+    }
+
+    persistCartItems() {
+        this.storage.setItem('cartItems', JSON.stringify(this.cartItems));
     }
 
     logCartData(totalPriceValue: number, totalQuantityValue: number) {
